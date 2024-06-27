fc058a99d51de2bed5fad937827e6f6ce0769184
Tejas <tejashtejas26@gmail.com>
2024-06-27 15:10:15 +0530

    ...

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -1,7 +1,6 @@
 /** whole apps navigation file */
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { TailwindProvider } from 'tailwindcss-react-native';
 import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
