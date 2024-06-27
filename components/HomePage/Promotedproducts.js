6c7be634e2d59b2ad0fc4241ab01337cb5b82c90
Rahul Pradhan <rahulp0817@gmail.com>
2024-06-27 12:28:37 +0530

    Rahul changes

--- components/HomePage/Promotedproducts.js
+++ components/HomePage/Promotedproducts.js
@@ -1,23 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, Pressable, Image } from 'react-native'
 import React from 'react'
-import promoted1 from '../../assets/promoted/promoted1.png'; 
-import promoted2 from '../../assets/promoted/promoted2.png'; 
-import promoted4 from '../../assets/promoted/promoted4.png'; 
+import { useFonts } from 'expo-font'
+import promoted1 from '../../assets/promoted/promoted1.png';
+import promoted2 from '../../assets/promoted/promoted2.png';
+import promoted4 from '../../assets/promoted/promoted4.png';
 
 const Promotedproducts = () => {
+
+  let [fontsLoaded] = useFonts({
+    'Nunito-SemiBold': require('../../assets/fonts/Nunito-SemiBold.ttf'),
+    'Nunito-Medium': require('../../assets/fonts/Nunito-Medium.ttf'),
+    'Nunito-Regular': require('../../assets/fonts/Nunito-Regular.ttf'),
+
+  })
 
   const deal = [
     {
       id: '0',
       title: 'Worldwide Treats',
-      des: 'All Types of Grocery',
+      des: 'Jeans large size',
       image: promoted1,
-      
+
     },
     {
       id: '1',
       title: 'Trendy T-shirt',
-      des: '7, colors all size',
+      des: '7 colors, All Size',
       image: promoted2,
     },
     {
@@ -31,13 +39,35 @@ const Promotedproducts = () => {
       title: 'Women\'s T-shirt',
       des: 'shirts of women',
       image: promoted1,
+    },
+    {
+      id: '4',
+      title: 'Women\'s T-shirt',
+      des: 'shirts of women',
+      image: promoted1,
+
     }
   ]
 
   return (
-    <View>
-      <Text>Promotedproducts</Text>
-    </View>
+    <>
+      <Text style={{ fontSize: 20, fontWeight: 600, fontFamily: 'Nunito-SemiBold', margin: 20, marginBottom:10 }}>Promoted</Text>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{ margin: 10, marginTop: 5 }}>
+        {deal.map((item, index) => (
+          <Pressable key={index} style={{ borderColor: '#E5E6E6', margin: 10, borderWidth: 1, borderRadius: 4, justifyContent: "center", alignItems: "center", padding:4}}>
+            <View style={{ justifyContent: "center", backgroundColor: 'white',  }}>
+              <Image
+                source={item.image}
+                style={{ width: 142, height: 142, borderRadius: 4, }}
+              />
+              <Text numberOfLines={1} style={{ fontSize: 12, marginTop: 8, fontWeight: 600, marginLeft:10 }}>{item.title}</Text>
+              <Text numberOfLines={1} style={{fontSize: 12, fontWeight: 400, color: '#7C7E7E', marginBottom:10, marginLeft:10 }}>{item.des}</Text>
+            </View>
+          </Pressable>
+        ))}
+
+      </ScrollView>
+    </>
   )
 }
 
--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -1,7 +1,6 @@
-import React from 'react'
+import { useRef, React } from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { TailwindProvider } from 'tailwindcss-react-native';
-import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native'
+import { KeyboardAvoidingView, Text, Animated, Dimensions } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -23,6 +22,8 @@ import More from '../screens/MainPages/More';
 import OTPScreen from '../screens/HomePages/Modal/OTPScreen'
 import Accountcreate from '../screens/HomePages/Modal/Accountcreate'
 import RegisterScreen from '../screens/HomePages/Modal/RegisterScreen'
+import SplashScreen from '../components/Splash/SplashScreen';
+
 
 
 const StackNavigator = () => {
@@ -31,79 +32,136 @@ const StackNavigator = () => {
   const Stack = createNativeStackNavigator();
   const Tab = createBottomTabNavigator();
 
+
+  // Animated
+  const tabOffsetValue = useRef(new Animated.Value(0)).current;
+
+  const getWidth = () => {
+    let width = Dimensions.get('window').width;
+    width = width - 0;
+    return width / 4;
+  };
+
+  const handleTabPress = (index) => {
+    Animated.spring(tabOffsetValue, {
+      toValue: getWidth() * index,
+      useNativeDriver: true,
+    }).start();
+  };
+
+
   const BottomTabs = () => {
-    return(
-      <Tab.Navigator
-       screenOptions={{keyboardHidesTabBar: true, style:{ position: 'absolute'}}}>
-        <Tab.Screen name="Home" component={HomeScreen} 
-        options={{
-          tabBarLabel: "Home",
-          tabBarLabelStyle: {color: "#008E97"},
-          headerShown: false,
-          tabBarIcon: ({ focused }) =>
-            focused ? (
-              <HomeIconActive name="Home" width={24} />
-            ) : (
-              <HomeIconInactive name="Home" width={24} />
-            ),
-        }} />
-        <Tab.Screen name="Reels" component={Reels} 
-        options={{
-          tabBarLabel: "Reels",
-          tabBarLabelStyle: {color: "#008E97"},
-          headerShown: false,
-          tabBarIcon: ({ focused }) =>
-            focused ? (
-              <ReelsIconActive name="Reels" width={24} />
-            ) : (
-              <ReelsIconInactive name="Reels" width={24} />
-            ),
-        }} />
-        <Tab.Screen name="MyGroups" component={MyGroups} 
-        options={{
-          tabBarLabel: "MyGroups",
-          tabBarLabelStyle: {color: "#008E97"},
-          headerShown: true,
-          tabBarIcon: ({ focused }) =>
-            focused ? (
-              <GroupIconActive name="MyGroups" width={24}  />
-            ) : (
-              <GroupsIconInactive name="MyGroups" width={24} />
-            ),
-        }} />
-        <Tab.Screen name="More" component={More} 
-        options={{
-          tabBarLabel: "More",
-          tabBarLabelStyle: {color: "#008E97"},
-          headerShown: false,
-          tabBarIcon: ({ focused }) =>
-            focused ? (
-              <MoreIconActive name="More" width={24}  />
-            ) : (
-              <MoreIconInactive name="More" width={24} />
-            ),
-        }} />
-      </Tab.Navigator>
+    return (
+      <>
+        <Tab.Navigator
+          screenOptions={{ keyboardHidesTabBar: true, style: { position: 'absolute' } }}>
+          <Tab.Screen name="Home" component={HomeScreen}
+            options={{
+              tabBarLabel: ({ focused }) => (
+                <Text style={{ fontSize: 10, color: focused ? "#347B72" : "#636767" }}>Home</Text>
+              ),
+              headerShown: false,
+              tabBarIcon: ({ focused }) =>
+                focused ? (
+                  <HomeIconActive name="Home" width={24} />
+                ) : (
+                  <HomeIconInactive name="Home" width={24} />
+                ),
+            }}
+            listeners={() => ({
+              tabPress: () => handleTabPress(0),
+            })}
+          />
+          <Tab.Screen name="Reels" component={Reels}
+            options={{
+              tabBarLabel: ({ focused }) => (
+                <Text style={{ fontSize: 10, color: focused ? "#347B72" : "#636767" }}>Reels</Text>
+              ),
+              headerShown: false,
+              tabBarIcon: ({ focused }) =>
+                focused ? (
+                  <ReelsIconActive name="Reels" width={24} />
+                ) : (
+                  <ReelsIconInactive name="Reels" width={24} />
+                ),
+            }}
+            listeners={() => ({
+              tabPress: () => handleTabPress(1),
+            })}
+          />
+          <Tab.Screen name="MyGroups" component={MyGroups}
+            options={{
+              tabBarLabel: ({ focused }) => (
+                <Text style={{ fontSize: 10, color: focused ? "#347B72" : "#636767" }}>MyGroups</Text>
+              ),
+              headerShown: true,
+              tabBarIcon: ({ focused }) =>
+                focused ? (
+                  <GroupIconActive name="MyGroups" width={24} />
+                ) : (
+                  <GroupsIconInactive name="MyGroups" width={24} />
+                ),
+            }}
+            listeners={() => ({
+              tabPress: () => handleTabPress(2),
+            })}
+          />
+          <Tab.Screen name="More" component={More}
+            options={{
+              tabBarLabel: ({ focused }) => (
+                <Text style={{ fontSize: 10, color: focused ? "#347B72" : "#636767" }}>More</Text>
+              ),
+              headerShown: true,
+              tabBarIcon: ({ focused }) =>
+                focused ? (
+                  <MoreIconActive name="More" width={24} />
+                ) : (
+                  <MoreIconInactive name="More" width={24} />
+                ),
+            }}
+            listeners={() => ({
+              tabPress: () => handleTabPress(3),
+            })}
+          />
+        </Tab.Navigator>
+
+
+        <Animated.View style={{
+          width: getWidth() - 30,
+          height: 4,
+          backgroundColor: '#347B72',
+          position: 'absolute',
+          borderTopLeftRadius: 0,
+          borderTopRightRadius: 0,
+          borderBottomLeftRadius: 16,
+          borderBottomRightRadius: 16,
+          bottom: 45,
+          left: 14,
+          transform: [{ translateX: tabOffsetValue }]
+        }}
+        />
+
+      </>
     )
   }
   return (
     <NavigationContainer>
-      <StatusBar backgroundColor="white" barStyle="dark-content" /> 
-      <TailwindProvider>
-        <Stack.Navigator initialRouteName='Main'>
-          <Stack.Screen name="Main" component={BottomTabs} options={{ headerShown: false }} />
-          <Stack.Screen name="AccountCreate" component={Accountcreate} options={{ headerShown: true, headerTitle: ''  }}/> 
-          <Stack.Screen name="OTPScreen" component={OTPScreen} options={{ headerShown: true, headerTitle: '' }}/>
-          <Stack.Screen name="RegisterScreen" component={RegisterScreen} options={{ headerShown: true}}/>
-          <Stack.Screen name="Wishlist" component={Wishlist} options={{ headerShown: true }}/>
-          <Stack.Screen name="Notifications" component={Notifications} options={{ headerShown: true }}/>
-        </Stack.Navigator>
-      </TailwindProvider>
+      <StatusBar backgroundColor="white" barStyle="dark-content" />
+      <Stack.Navigator initialRouteName='Splash'>
+        <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Main" component={BottomTabs} options={{ headerShown: false }} />
+        <Stack.Screen name="AccountCreate" component={Accountcreate} options={{ headerShown: true, headerTitle: '' }} />
+        <Stack.Screen name="OTPScreen" component={OTPScreen} options={{ headerShown: true, headerTitle: '' }} />
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} options={{ headerShown: true }} />
+        <Stack.Screen name="Wishlist" component={Wishlist} options={{ headerShown: true }} />
+        <Stack.Screen name="Notifications" component={Notifications} options={{ headerShown: true }} />
+      </Stack.Navigator>
     </NavigationContainer>
   )
 }
 
+
+
 export default StackNavigator
 
-const styles = StyleSheet.create({})
 //rnfes
